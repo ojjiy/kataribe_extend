4e319cd10461261b17b6146b429ea193276f4cfc
Yuta.O <yuta@Yuta-mbp.local>
2019-09-28 20:44:33 +0900

    remove reference to original files

--- reshape.py
+++ reshape.py
@@ -5,6 +5,20 @@ import glob
 import parse
 import copy
 
+## Constants
+TIMER_UNIT_DEF = "Timer unit: {unit:g} s"
+TIMER_UNIT_LINE = 0
+TOTAL_TIME_DEF = "Total time: {time:g} s"
+TOTAL_TIME_LINE = 2
+FILE_NAME_DEF = "File: {fname:S}"
+FILE_NAME_LINE = 3
+FUNC_NAME_DEF = "Function: {func_name:S}{:.}"
+FUNC_NAME_LINE = 4
+FUNC_LINE_DEF = "{:.}at line {line_num:d}"
+FUNC_LINE_LINE = 4
+
+COLUMN_NAME_LINE = 6
+CODE_COLUMN_NAME = "Line Contents"
 
 class FileInfo():
     def __init__(self, filename=None):
@@ -16,17 +30,18 @@ class FileInfo():
             with open(filename) as f:
                 data = f.readlines()
             self.content['unit'] = parse.parse(
-                "Timer unit: {:g} s", data[0])[0]
+                TIMER_UNIT_DEF, data[TIMER_UNIT_LINE])['unit']
             self.content['time'] = parse.parse(
-                "Total time: {:g} s", data[2])[0]
-            self.content['fname'] = parse.parse("File: {:S}", data[3])[0]
+                TOTAL_TIME_DEF, data[TOTAL_TIME_LINE])['time']
+            self.content['fname'] = parse.parse(FILE_NAME_DEF, data[FILE_NAME_LINE])['fname']
             self.content['func_name'] = parse.parse(
-                "Function: {:S} at line {:d}", data[4])[0]
+                FUNC_NAME_DEF, data[FUNC_NAME_LINE])['func_name']
             self.content['line_num'] = parse.parse(
-                "Function: {:S} at line {:d}", data[4])[1]
+                FUNC_LINE_DEF, data[FUNC_LINE_LINE])['line_num']
 
             self.content['res'] = {}
-            for l in (x.strip() for x in data[8:]):
+            self.code_offset = data[COLUMN_NAME_LINE].find(CODE_COLUMN_NAME)
+            for l in (x.rstrip("\n") for x in data[8:]):
                 if l == "":
                     break
                 chunk = l.split()
@@ -37,8 +52,9 @@ class FileInfo():
                 line_dict = parse.parse(
                     "{hits:d}{:s}{time:f}{:s}{perhit:f}{:s}{ratio:f}{:s}{code}", " ".join(chunk[1:]))
                 if line_dict is None:
-                    self.content['res'][line]['code'] = " ".join(chunk[1:])
+                    self.content['res'][line]['code'] = l[self.code_offset:]
                     continue
+                line_dict.named['code'] = l[self.code_offset:]
                 self.content['res'][line] = line_dict.named
                 self.maxim_hits_digits = max(self.maxim_hits_digits, len(
                     str(self.content['res'][line]['hits'])))
@@ -66,9 +82,8 @@ class FileInfo():
                     .format(line_num="Line #", hits="Hits", hits_digits=self.maxim_hits_digits+1, time="Time", time_digits=self.maxim_time_digits+3, per_hit="Per Hit", ratio="% Time"))
             f.write("="*(6+9+9+self.maxim_time_digits +
                          self.maxim_hits_digits+4+15)+"\n")
-            with open(self.content['fname'], 'r') as f2:
-                src = f2.readlines()
             for line in self.content['res'].keys():
+                code = self.content['res'][line]['code']+"\n" if 'code' in self.content['res'][line].keys() else "\n"
                 if 'hits' in self.content['res'][line].keys():
                     if colorize:
                         f.write(self.colorize(
@@ -77,10 +92,10 @@ class FileInfo():
                             .format(line_num=line, hits=self.content['res'][line]['hits'], hits_digits=self.maxim_hits_digits+1, time=self.content['res'][line]['time'], time_digits=self.maxim_time_digits+3, per_hit=self.content['res'][line]['perhit'], ratio=self.content['res'][line]['ratio']))
                     if colorize:
                         f.write("\033[m")
-                    f.write(src[line-1])
+                    f.write(code)
                 else:
                     f.write("{line_num:>6}{hits:>{hits_digits}}{time:>{time_digits}}{per_hit:>9}{ratio:>9}  {code}"
-                            .format(line_num=line, hits="", hits_digits=self.maxim_hits_digits+1, time="", time_digits=self.maxim_time_digits+3, per_hit="", ratio="", code=src[line-1]))
+                            .format(line_num=line, hits="", hits_digits=self.maxim_hits_digits+1, time="", time_digits=self.maxim_time_digits+3, per_hit="", ratio="", code=code))
 
     def __add__(self, other):
         if not self.isAddable(other):
@@ -121,6 +136,7 @@ class FileInfo():
                 result.content['res'][num]['hits']
             result.content['res'][num]['ratio'] = result.content['res'][num]['time'] * \
                 result.content['unit'] * 100 / result.content['time']
+        result.code_offset = 31 + self.maxim_hits_digits + self.maxim_time_digits
         return result
 
     def __radd__(self, other):
